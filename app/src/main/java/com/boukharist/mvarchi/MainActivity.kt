4e3ed907c3b5bc567fe1d97bd0e7d4bb09654a20
Ayoub Boukhari <boukhari.ayoub14@gmail.com>
2020-03-08 01:22:23 +0100

    step_1

--- app/src/main/java/com/boukharist/mvarchi/MainActivity.kt
+++ app/src/main/java/com/boukharist/mvarchi/MainActivity.kt
@@ -1,10 +1,8 @@
 package com.boukharist.mvarchi
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.form_view.*
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.info_view.*
-import java.time.LocalDateTime
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,38 +12,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         userApi = UserApi()
         populateData()
-        setListeners()
-    }
-
-    private fun setListeners() {
-        validateButton.setOnClickListener {
-            val firstName = firstNameText.editText!!.text.toString()
-            val lastName = lastNameText.editText!!.text.toString()
-            val birthDateString = birthDateText.editText!!.text.toString()
-            val user = User(firstName,lastName,birthDateString)
-            setCurrentUser(user)
-        }
-    }
-
-    private fun setCurrentUser(user: User) {
-        userApi.setUser(user)
-        populateData()
     }
 
     private fun populateData() {
         val user = userApi.getUser()
-        setAge(user)
-        setName(user)
+        showName(user)
     }
 
-    private fun setName(user: User) {
+    private fun showName(user: User) {
         val fullName = user.getFullName()
         nameTextView.text = getString(R.string.full_name_placeholder, fullName)
-    }
-
-    private fun setAge(user: User) {
-        val now = LocalDateTime.now()
-        val age = user.getAge(now)
-        ageTextView.text = getString(R.string.age_placeholder, age)
     }
 }
--- app/src/main/java/com/boukharist/mvarchi/UserModel.kt
+++ app/src/main/java/com/boukharist/mvarchi/UserModel.kt
@@ -1,27 +1,15 @@
 package com.boukharist.mvarchi
 
-import java.time.LocalDate
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
-import java.time.temporal.ChronoUnit
-
 data class User(
     private val firstName: String,
-    private val lastName: String,
-    private val birthDate: String
+    private val lastName: String
 ) {
-
-    fun getAge(now: LocalDateTime): Int {
-        val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
-        val date2: LocalDateTime = LocalDate.parse(birthDate, formatter).atStartOfDay()
-        return ChronoUnit.YEARS.between(date2, now).toInt()
-    }
 
     fun getFullName() = "$firstName $lastName"
 }
 
 class UserApi {
-    private var user: User = User("Kanye", "West", "11/07/1990")
+    private var user: User = User("Kanye", "West")
 
     fun setUser(user: User) {
         this.user = user
